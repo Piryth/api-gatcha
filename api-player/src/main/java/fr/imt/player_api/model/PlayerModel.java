a08d4cce068ebcbcb3c1ae7097ed7ae5c6b3a334
villett <teo.villet@gipsa-lab.grenoble-inp.fr>
2025-02-11 10:43:58 +0100

    use lombook for playerModel

--- api-player/src/main/java/fr/imt/player_api/model/PlayerModel.java
+++ api-player/src/main/java/fr/imt/player_api/model/PlayerModel.java
@@ -2,61 +2,33 @@ package fr.imt.player_api.model;
 
 
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Getter
 @Document(collection = "players")
 public class PlayerModel {
 
     @Id
     private String id;
+    @Setter
     @Size(max = 50)
     private int level;
+    @Setter
     private int exp;
+    @Setter
     private int curr_exp;
+    @Setter
     private List<String> monsters;
 
     public PlayerModel(int level, int exp, int curr_exp, List<String> monsters) {
         this.level = level;
         this.exp = exp;
         this.curr_exp = curr_exp;
-        this.monsters = monsters;
-    }
-
-    public String getId(){
-        return id;
-    }
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public int getExp() {
-        return exp;
-    }
-
-    public void setExp(int exp) {
-        this.exp = exp;
-    }
-
-    public int getCurr_exp() {
-        return curr_exp;
-    }
-
-    public void setCurr_exp(int curr_exp) {
-        this.curr_exp = curr_exp;
-    }
-
-    public List<String> getMonsters() {
-        return monsters;
-    }
-
-    public void setMonsters(List<String> monsters) {
         this.monsters = monsters;
     }
 
