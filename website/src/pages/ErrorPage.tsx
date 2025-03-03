3bab483d6a9916162f4441e44d3d919d23630847
teovlt <teo.villet2@gmail.com>
2025-03-03 16:44:55 +0100

    delete website from develop branch

--- website/src/pages/ErrorPage.tsx
+++
@@ -1,3 +0,0 @@
-export const ErrorPage = () => {
-  return <div className='flex justify-center items-center h-screen text-8xl'>404 - Page not found</div>;
-};
--- website/src/pages/Monstres.tsx
+++
@@ -1,7 +0,0 @@
-export const Monstres = () => {
-  return (
-    <div className='container mx-auto p-4'>
-      <h1 className='text-3xl font-bold text-primary'>Page Monstres</h1>
-    </div>
-  );
-};
