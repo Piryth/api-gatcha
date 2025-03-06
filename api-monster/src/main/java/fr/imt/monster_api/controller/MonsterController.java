90726b57af9865ac5c23208b385ea880474fc2f1
teovlt <teo.villet2@gmail.com>
2025-03-06 16:19:24 +0100

    delete unused corsOrigin config in apis

--- api-monster/src/main/java/fr/imt/monster_api/controller/MonsterController.java
+++ api-monster/src/main/java/fr/imt/monster_api/controller/MonsterController.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/monster-api/v1/monsters")
-@CrossOrigin(origins = "http://localhost:5173")
 public class MonsterController {
 
     private final MonsterService monsterService;
