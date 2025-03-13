9d4374aedd55ee171e11a013eb3921113c8a2bac
teovlt <teo.villet2@gmail.com>
2025-03-13 10:36:58 +0100

    Add intercepters into the axios config to send the headers correctly

--- website/src/config/axiosConfig.tsx
+++ website/src/config/axiosConfig.tsx
@@ -1,10 +1,26 @@
 import axios from 'axios';
 
+// Create an instance of Axios with base configuration
 export const axiosConfig = axios.create({
   baseURL: 'http://localhost:8888',
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${document.cookie.split('=')[1]}`,
   },
 });
+
+// Add a request interceptor to set the Authorization header dynamically
+axiosConfig.interceptors.request.use(
+  (config) => {
+    // Retrieve the token from the cookie
+    const token = document.cookie.split('=')[1];
+    if (token) {
+      // Set the Authorization header with the token
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
